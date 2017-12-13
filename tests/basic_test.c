d478f90d0bffde0c28ae1da3920be6589311e003
Razshal <fontenille.mathieu@gmail.com>
2017-12-13 14:03:26 +0100

    commit before lunch

--- tests/basic_test.c
+++ tests/basic_test.c
@@ -6,7 +6,7 @@
 /*   By: mfonteni <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/08 13:03:48 by mfonteni          #+#    #+#             */
-/*   Updated: 2017/12/11 19:45:27 by mfonteni         ###   ########.fr       */
+/*   Updated: 2017/12/13 13:34:34 by mfonteni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,12 +18,35 @@ void	basic_test(void)
 	char	*line;
 	int fd = open("./tests/test4", O_RDONLY);
 
-	printf("1st return :%d\n", get_next_line(fd, &line));
+	get_next_line(fd, &line);
 	printf("line = %s\n", line);
 	free(line);
+
 	get_next_line(fd, &line);
-	free(line);
 	printf("line2 = %s\n", line);
+	free(line);
+	line = NULL;
+
 	get_next_line(fd, &line);
 	printf("line3 = %s\n", line);
+	free(line);
+
+	get_next_line(fd, &line);
+	printf("line4 = %s\n", line);
+	free(line);
+
+	printf("return:%d\n", get_next_line(fd, &line));
+	printf("line5 = %s\n", line);
+	free(line);
+
+///////////////=================================================
+	fd = open("./tests/test5", O_RDONLY);
+
+	get_next_line(fd, &line);
+	printf("line = %s\n", line);
+	free(line);
+
+	get_next_line(fd, &line);
+	printf("line2 = %s\n", line);
+	free(line);
 }
