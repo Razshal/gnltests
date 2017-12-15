d23d3090f25767941209c46f829e98dd5ee356b1
Razshal <fontenille.mathieu@gmail.com>
2017-12-15 20:17:37 +0100

    basic test refactored

--- tests/basic_test.c
+++ tests/basic_test.c
@@ -6,101 +6,62 @@
 /*   By: mfonteni <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/08 13:03:48 by mfonteni          #+#    #+#             */
-/*   Updated: 2017/12/15 18:31:18 by mfonteni         ###   ########.fr       */
+/*   Updated: 2017/12/15 20:16:58 by mfonteni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../gnltests.h"
 
-static int	generic_tester(int condition)
+char	*line = NULL;
+
+static int	generic_tester(char *expected, int fd)
 {
-	if (condition)
+	GNLRETURN(get_next_line(fd, &line));
+	if (line && !strcmp(line, expected))
 	{
 		PRINTFSUCCESS;
 		return (1);
 	}
 	else
+	{
 		PRINTFFAILURE;
+		printf("your line =\n|%s|\ninstead of\n|%s|\n", line, expected);
+	}
 	return (0);
 }
 
 void	basic_test(void)
 {
 	PRINTNAME("basic_test");
-	char	*line = NULL;
-	char	*line2 = NULL;
+//	char	*line = NULL;
 	int fd = open("./tests/test1", O_RDONLY);
-
-	get_next_line(fd, &line);
-
-	if (!generic_tester(!strcmp(line, "teeeeest")))
-		printf("|%s|\ninstead of\n|teeeeest|\n", line);
-
+	generic_tester("teeeeest", fd);
 	ft_memdel((void**)&line);
 
 ///////////////=================================================
 
 	fd = open("./tests/test2", O_RDONLY);
 	GNLRETURN(get_next_line(fd, &line));
-
-	if (!generic_tester(!strcmp(line, "1234fjdskfjdskfjdskjfhds1")))
-		printf("you are returning\n|%s|\ninstead of\n|teeeeest|\n", line);
-
+	generic_tester("1234fjdskfjdskfjdskjfhds1", fd);
 	ft_memdel((void**)&line);
+	close(fd);
 
 ///////////////================================================
 
-	fd = open("./tests/gnl7_3", O_RDONLY);
-	GNLRETURN(get_next_line(fd, &line));
-
-	if (!generic_tester(line && !strcmp(line, "1234567")))
-		printf("you are returning\n|%s|\ninstead of\n|teeeeest|\n", line);
-
+	fd = open("./tests/gnl7_3.txt", O_RDONLY);
+	generic_tester("1234567", fd);
+	generic_tester("abcdefg", fd);
+	generic_tester("4567890", fd);
+	generic_tester("defghijk", fd);
 	ft_memdel((void**)&line);
 
 ///////////////=================================================
 
-	fd = open("./tests/gnl7_2", O_RDONLY);
+	fd = open("./tests/gnl7_2.txt", O_RDONLY);
 	GNLRETURN(get_next_line(fd, &line));
-
-	if (!generic_tester(line && !strcmp(line, "1234567")))
-		printf("you are returning\n|%s|\ninstead of\n|teeeeest|\n", line);
-
+	generic_tester("1234567", fd);
 	ft_memdel((void**)&line);
+	close(fd);
 
 ///////////////=================================================
-
-	printf("gnl ret %d\nline2 = %s\n", get_next_line(fd, &line), line);
-	if (line)free(line);
-	line = NULL;
-
-	printf("gnl ret %d\nline3 = %s\n", get_next_line(fd, &line), line);
-	if (line)free(line);
-	line = NULL;
-
-	printf("gnl ret %d\nline4 = %s\n", get_next_line(fd, &line), line);
-	if (line)free(line);
-	line = NULL;
-
-	printf("gnl ret %d\nline5 = %s\n", get_next_line(fd, &line), line);
-	if (line)free(line);
-	line = NULL;
-	
-	printf("read:%d\n", get_next_line(42, &line));
-	if (line)free(line);
-	line = NULL;
-
-///////////////=================================================
-	PRINTNAME("test2");
-	fd = open("./tests/test3", O_RDONLY);
-
-	get_next_line(fd, &line2);
-	printf("line = %s\n", line2);
-	if (line2)free(line2);
-	line2 = NULL;
-
-	get_next_line(fd, &line2);
-	printf("line2 = %s\n", line2);
-	if (line2)free(line2);
-	line2 = NULL;
 }
