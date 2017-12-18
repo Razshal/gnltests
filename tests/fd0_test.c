fb7171873a55ae1c7cca4524d0816fd0ec7d4f15
Razshal <fontenille.mathieu@gmail.com>
2017-12-18 12:09:32 +0100

    better fd0_test

--- tests/fd0_test.c
+++ tests/fd0_test.c
@@ -6,7 +6,7 @@
 /*   By: mfonteni <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/17 19:13:35 by mfonteni          #+#    #+#             */
-/*   Updated: 2017/12/17 19:56:27 by mfonteni         ###   ########.fr       */
+/*   Updated: 2017/12/18 12:09:02 by mfonteni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,11 @@
 void	fd0_test(void)
 {
 	char	*str;
-	int		res = 0;
+	int		res = 5;
 
-	while ((res = get_next_line(0, &str)))
+	while (res > 0)
 	{
+		res = get_next_line(0, &str);
 		printf("Return : %d\n", res);
 		printf("Line:\n%s\n", str);
 	}
